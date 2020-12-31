8e9be591db2a6cf819d978b6741d5c327cce15c6
rockytechtips <i.rakeshraj1995@gmail.com>
2020-12-31 18:10:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const port = process.env.PORT || "3000";
+
+// set default folder for importing css,jsand images
+app.use(express.static(__dirname + '/public'));
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("home", { title: "Home" });
+});
+app.get("/contact", function (req, res) {
+  res.render("contact", { title: "Contact" });
+});
+
+app.listen(port, function () {
+  console.log("server is listening at port", port);
+});
